3314998dcabecb7dafa5824227744e79a89b0b83
Betinsk <gmdust@hotmail.com>
2021-08-05 20:16:48 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.nelioalves.cursomc.config;
 
+import com.nelioalves.cursomc.security.JWTAuthorizationFilter;
+import com.nelioalves.cursomc.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +23,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -28,6 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     Environment env;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
 
     private static final String[] PUBLIC_MATCHERS = {
             "/h2-console/**",
@@ -35,8 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/produtos/**",
-            "/categorias/**",
-            "/clientes/**"
+            "/categorias/**"
+    };
+
+
+    private static final String[] PUBLIC_MATCHERS_POST = {
+           "/clientes/**"
     };
 
     @Override
@@ -47,9 +58,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       }
        http.cors().and().csrf().disable();
         http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
          .antMatchers(PUBLIC_MATCHERS).permitAll()
          .anyRequest().authenticated();
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
