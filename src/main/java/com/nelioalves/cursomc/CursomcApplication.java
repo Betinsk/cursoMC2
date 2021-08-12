ad573816826c3096693655b99632874769df6c1e
Betinsk <gmdust@hotmail.com>
2021-08-12 20:49:19 -0300

    AWS Services

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.services.S3Service;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
